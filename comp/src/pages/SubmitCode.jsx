c30e060f0f50eb4fea3771c295f0f86bbb08e448
Raj <39922631+raj-28@users.noreply.github.com>
2023-04-14 11:52:29 +0000

    so did msg and updation

--- comp/src/pages/SubmitCode.jsx
+++ comp/src/pages/SubmitCode.jsx
@@ -100,92 +100,124 @@
 // -------------------
 
 import React, { useState } from "react";
-import "../styles/Previewcard.css";
+import { useNavigate } from "react-router-dom";
+import "../styles/SubmitCode.css";
 import VerifyCode from "../components/VerifyCode";
 
 function SubmitCode() {
+  // Initialize state variables for the form
   const [selectedLanguage, setSelectedLanguage] = useState("");
   const [code, setCode] = useState("");
   const [isValid, setIsValid] = useState(false);
   const [title, setTitle] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
+  // Get the navigate function from the react-router-dom package
+  const navigate = useNavigate();
+
+  // Update the selectedLanguage state variable when the user selects a language
   const handleLanguageChange = (event) => {
     setSelectedLanguage(event.target.value);
-    setIsValid(false);
+    setIsValid(false); // Reset the validation flag
   };
 
+  // Update the code state variable when the user types into the code input field
   const handleCodeChange = (event) => {
     setCode(event.target.value);
-    setIsValid(false);
+    setIsValid(false); // Reset the validation flag
   };
 
+  // Update the title state variable when the user types into the title input field
   const handleTitleChange = (event) => {
     setTitle(event.target.value);
-    setIsValid(false);
+    setIsValid(false); // Reset the validation flag
   };
 
+  // Set the isValid state variable to true when the user clicks the Verify button
   const handleCodeVerify = () => {
     setIsValid(true);
   };
 
+  // Handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    setIsSubmitted(true); // Set the isSubmitted flag to true
+    // TODO: Perform submission logic here (e.g. send the form data to a server)
+    // Navigate to the code snippet page after successful submission
+    navigate("/code-snippet");
+  };
+
   return (
-    <div className="App">
-      <h1>Submit Code Snippet</h1>
-      <form>
-        <label htmlFor="title">Snippet Title:</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={title}
-          onChange={handleTitleChange}
-        />
-        <br />
-        <br />
-        <label htmlFor="language">Language:</label>
-        <select
-          id="language"
-          name="language"
-          value={selectedLanguage}
-          onChange={handleLanguageChange}
-        >
-          <option value="">Select Language</option>
-          <option value="React">React</option>
-          <option value="JavaScript">JavaScript</option>
-          <option value="Node">Node</option>
-          <option value="HTML">HTML</option>
-        </select>
-        <br />
-        <br />
-        <label htmlFor="code">Code:</label>
-        <textarea
-          id="code"
-          name="code"
-          rows="10"
-          cols="50"
-          value={code}
-          onChange={handleCodeChange}
-        ></textarea>
-        <br />
-        <br />
-        <button type="button" onClick={handleCodeVerify}>
+    <div className="container">
+      {/* Display the form heading */}
+      <h1 className="heading">Submit Code Snippet</h1>
+      {/* Display the form */}
+      <form className="form" onSubmit={handleSubmit}>
+        {/* Display the title input field */}
+        <div className="form-group">
+          <label htmlFor="title">Snippet Title:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={title}
+            onChange={handleTitleChange}
+            className="input"
+          />
+        </div>
+        {/* Display the language dropdown */}
+        <div className="form-group">
+          <label htmlFor="language">Language:</label>
+          <select
+            id="language"
+            name="language"
+            value={selectedLanguage}
+            onChange={handleLanguageChange}
+            className="select"
+          >
+            <option value="">Select Language</option>
+            <option value="React">React</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="Node">Node</option>
+            <option value="HTML">HTML</option>
+          </select>
+        </div>
+        {/* Display the code input field */}
+        <div className="form-group">
+          <label htmlFor="code">Code:</label>
+          <textarea
+            id="code"
+            name="code"
+            rows="10"
+            cols="50"
+            value={code}
+            onChange={handleCodeChange}
+            className="textarea"
+          ></textarea>
+        </div>
+        {/* Display the Verify button */}
+        <button type="button" onClick={handleCodeVerify} className="button">
           Verify
         </button>
-        <br />
-        <br />
+        {/* Display the verification results */}
         <VerifyCode
-          selectedLanguage={selectedLanguage}
-          code={code}
-          isValid={isValid}
-        />
-        <br />
-        <br />
-        <button type="submit" disabled={!isValid}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+       selectedLanguage={selectedLanguage}
+       code={code}
+       isValid={isValid}
+     />
+{/* Display the submit button */}
+<button type="submit" disabled={!isValid} className="button">
+Submit
+</button>
+</form>
+</div>
+);
 }
 
 export default SubmitCode;
+
+
+
+
+
+
