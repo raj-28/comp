889e5d53df96c6f74f8f29b49fc43249812a043d
Raj <39922631+raj-28@users.noreply.github.com>
2023-04-14 10:13:32 +0000

    now updated submit code snippet area

--- comp/src/pages/SubmitCode.jsx
+++ comp/src/pages/SubmitCode.jsx
@@ -1,16 +1,92 @@
-function SubmitCode() {
-    return (
-      <div>
-        <h1>Welcome to our Submit code snippets website!</h1>
-        <p>
-          Here you'll find a collection of helpful code snippets that you can use
-          in your own projects. Feel free to browse through our collection, submit
-          your own code snippets, and read articles on programming and web
-          development.
-        </p>
-      </div>
-    );
-  }
-  
-  export default SubmitCode;
-  
+import React, { useState } from "react";
+import "../styles/Previewcard.css";
+
+function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [code, setCode] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+    setIsValid(false);
+  };
+
+  const handleCodeChange = (event) => {
+    setCode(event.target.value);
+    setIsValid(false);
+  };
+
+  const verifyCode = () => {
+    // Verify the language of the code
+    // This is a simplified example using only a few languages
+    let valid = false;
+    if (selectedLanguage === "javascript") {
+      valid = /^function|^const|^let|^var/.test(code);
+    } else if (selectedLanguage === "python") {
+      valid = /^def|^class/.test(code);
+    } else if (selectedLanguage === "java") {
+      valid = /^public\s+class|^public\s+static\s+void/.test(code);
+    } else if (selectedLanguage === "csharp") {
+      valid = /^public\s+class|^public\s+static\s+void/.test(code);
+    }
+
+    setIsValid(valid);
+  };
+
+  return (
+    <div className="App">
+      <h1>Submit Code Snippet</h1>
+      <form>
+        <label htmlFor="language">Language:</label>
+        <select
+          id="language"
+          name="language"
+          value={selectedLanguage}
+          onChange={handleLanguageChange}
+        >
+          <option value="">Select Language</option>
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+          <option value="csharp">C#</option>
+        </select>
+        <br />
+        <br />
+        <label htmlFor="code">Code:</label>
+        <textarea
+          id="code"
+          name="code"
+          rows="10"
+          cols="50"
+          value={code}
+          onChange={handleCodeChange}
+        ></textarea>
+        <br />
+        <br />
+        <button type="button" onClick={verifyCode}>
+          Verify
+        </button>
+        <br />
+        <br />
+        <div id="preview">
+          {isValid ? (
+            <pre>
+              <code className={selectedLanguage}>{code}</code>
+            </pre>
+          ) : (
+            selectedLanguage && (
+              <p>Invalid code for the selected language.</p>
+            )
+          )}
+        </div>
+        <br />
+        <br />
+        <button type="submit" disabled={!isValid}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
